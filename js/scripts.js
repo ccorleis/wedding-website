73a7713ff6d0e491ebc43446d259c7721c6550d3
Christoph Corleis <christoph.corleis@protonmail.com>
2022-10-02 16:54:57 +0200

    updated js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var url = 'https://script.google.com/macros/s/AKfycbyVdZzPtqAnsdeqR3TO40-Uasdz0KZbfIGLGLVIEfBVgc_SS57-rfScaTomA5DvEsns/exec'
+var url = 'https://script.google.com/macros/s/AKfycby3a04eEi3uQhH9NZYqo667Zg17TCJp09EMRRkXbJ0AquUMBxrArhpNMPh2x02ltDtS/exec'
 
 $(document).ready(function () {
     /********************** RSVP **********************/
@@ -6,24 +6,20 @@ $(document).ready(function () {
         e.preventDefault();
         var data = $(this).serialize();
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
-        alert("TEST");
-        if(is_valid_invitation_id(document.getElementById('invite_code').value)) {
-        
-            $.post(url, data)
-                    .done(function (data) {
-                        console.log(data);
-                        if (data.result === 'error') {
-                            $('#alert-wrapper').html(alert_markup('danger', data.message));
-                        } else {
-                            $('#alert-wrapper').html('');
-                            $('#rsvp-modal').modal('show');
-                        }
-                    })
-                    .fail(function (data) {
-                        console.log(data);
-                        $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
-                    });
-        }
+        is_valid_invitation_id(data);
+        $.post(url, data)
+                .done(function (data) {
+                    if (data.result === 'error') {
+                        $('#alert-wrapper').html(alert_markup('danger', data.message));
+                    } else {
+                        $('#alert-wrapper').html('');
+                        $('#rsvp-modal').modal('show');
+                    }
+                })
+                .fail(function (data) {
+                    console.log(data);
+                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+                });
     });
 });
 
@@ -31,7 +27,6 @@ function is_valid_invitation_id(data)
 {
     $.get(url, data)
         .done(function(data) {
-            alert(data.isValid);
             return data.isValid
           });
 }
