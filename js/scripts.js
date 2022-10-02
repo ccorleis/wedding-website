259f22179d1583dbbb56137cea0a6097bdcba9a4
Christoph Corleis <christoph.corleis@protonmail.com>
2022-10-02 15:26:48 +0200

    js updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,42 @@
-/********************** RSVP **********************/
-$('#rsvp-form').on('submit', function (e) {
-    e.preventDefault();
-    var data = $(this).serialize();
+var url = 'https://script.google.com/macros/s/AKfycbyVdZzPtqAnsdeqR3TO40-Uasdz0KZbfIGLGLVIEfBVgc_SS57-rfScaTomA5DvEsns/exec'
 
-    $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
-
-    $.post('https://script.google.com/macros/s/AKfycbwdE8YuoBVVUglGoIUQeA7Z98OtkJZZ3GRtJuzJhPy4Qpv-hXaMSAr9YSbRlj_ET5Q3/exec', data)
-            .done(function (data) {
-                console.log(data);
-                if (data.result === "error") {
-                    $('#alert-wrapper').html(alert_markup('danger', data.message));
-                } else {
-                    $('#alert-wrapper').html('');
-                    $('#rsvp-modal').modal('show');
-                }
-            })
-            .fail(function (data) {
-                console.log(data);
-                $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
-            });
+$(document).ready(function () {
+    /********************** RSVP **********************/
+    $('#rsvp-form').on('submit', function (e) {
+        e.preventDefault();
+        var data = $(this).serialize();
+        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
+        alert("TEST");
+        if(is_valid_invitation_id(document.getElementById('invite_code').value)) {
+        
+            $.post(url, data)
+                    .done(function (data) {
+                        console.log(data);
+                        if (data.result === 'error') {
+                            $('#alert-wrapper').html(alert_markup('danger', data.message));
+                        } else {
+                            $('#alert-wrapper').html('');
+                            $('#rsvp-modal').modal('show');
+                        }
+                    })
+                    .fail(function (data) {
+                        console.log(data);
+                        $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+                    });
+        }
+    });
 });
+
+function is_valid_invitation_id(data)
+{
+    $.get(url, data)
+        .done(function(data) {
+            alert(data.isValid);
+            return data.isValid
+          });
+}
+
+// alert_markup
+function alert_markup(alert_type, msg) {
+    return '<div class="alert alert-' + alert_type + '" role="alert">' + msg + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span>&times;</span></button></div>';
+}
