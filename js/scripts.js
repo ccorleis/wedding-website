b2cc7f99ddf5b60c11571a8b353c2ca579b70d6a
Christoph Corleis <christoph.corleis@protonmail.com>
2022-10-03 09:28:23 +0200

    updated js

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,30 @@ $(document).ready(function () {
     /********************** RSVP **********************/
     $('#rsvp-form').on('submit', function (e) {
         e.preventDefault();
+        Swal.showLoading();
         var data = $(this).serialize();
-        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
         $.post(url, data)
                 .done(function (data) {
+                    Swal.hideLoading()
                     if (data.result === 'error') {
-                        $('#alert-wrapper').html(alert_markup('danger', data.message));
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Error',
+                            showConfirmButton: false,
+                            timer: 1500
+                          })
                     } else {
-                        $('#alert-wrapper').html('');
-                        $('#rsvp-modal').modal('show');
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Your work has been saved',
+                            showConfirmButton: false,
+                            timer: 1500
+                          })
                     }
                 })
                 .fail(function (data) {
                     console.log(data);
-                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+                    alert_markup();
                 });
     });
 });
@@ -28,9 +38,4 @@ function is_valid_invitation_id(data)
         .done(function(data) {
             return data.isValid
           });
-}
-
-// alert_markup
-function alert_markup(alert_type, msg) {
-    return '<div class="alert alert-' + alert_type + '" role="alert">' + msg + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span>&times;</span></button></div>';
 }
